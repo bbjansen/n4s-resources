0f094399764a6059308ba8cebfc3d1e2e4985322
bbjansen <56940439+bbjansen@users.noreply.github.com>
2025-07-15 07:39:34 +0200

    Update setup_llama_rig.sh

--- setup_llama_rig.sh
+++ setup_llama_rig.sh
@@ -8,7 +8,8 @@ sudo apt install -y build-essential git cmake tmux wget unzip curl gnupg \
                     lsb-release software-properties-common python3-pip
 
 echo "==> Installing AMDGPU drivers with ROCm legacy..."
-wget https://repo.radeon.com/amdgpu-install/22.20/ubuntu/focal/amdgpu-install_22.20.50200-1_all.deb
+wget --referer http://support.amd.com https://drivers.amd.com/drivers/linux/amdgpu-pro-20.45-1188099-ubuntu-20.04.tar.xz
+
 sudo dpkg -i amdgpu-install_22.20.50200-1_all.deb
 sudo apt update
 sudo amdgpu-install --opencl=legacy,rocm --headless -y
